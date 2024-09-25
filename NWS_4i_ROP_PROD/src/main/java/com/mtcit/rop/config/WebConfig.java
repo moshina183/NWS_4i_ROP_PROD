9f74e68a24fe6a95e612873ee2c69cce88f1a997
Moshina_4iapps <moshina.ahamed@4iapps.com>
2024-09-25 16:52:02 +0530

    NWS_4i_ROP_PROD

---
+++ NWS_4i_ROP_PROD/src/main/java/com/mtcit/rop/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.mtcit.rop.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.mtcit.rop.interceptor.BaseInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Autowired
+    private BaseInterceptor baseInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(baseInterceptor);
+    }
+
+}
